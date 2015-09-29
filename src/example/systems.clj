5f9fac20d6c57f711a229d916bce542a87c8540c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-09-29 06:14:33 +0200

    whitespace

--- src/example/systems.clj
+++ src/example/systems.clj
@@ -1,7 +1,7 @@
 (ns example.systems
   (:require 
    [system.core :refer [defsystem]]
-   (system.components 
+   (system.components
     [http-kit :refer [new-web-server]]
     [h2 :refer [new-h2-database DEFAULT-MEM-SPEC DEFAULT-DB-SPEC]])
    [example.handler :refer [app]]
